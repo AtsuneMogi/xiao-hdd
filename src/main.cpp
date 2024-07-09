455bdccd47230828f6bc1dadcf5676391d521e05
AtsuneMogi <maomudunyin@gmail.com>
2024-07-09 14:32:26 +0900

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 
 int pin1 = 8;
+
+
 void freq(int pin, int f, int t) {
     tone(pin, f);
     delay(t - 40);
@@ -11,54 +13,54 @@ void freq(int pin, int f, int t) {
 
 
 void music_main() {
-    freq(pin1, 880, 250); // la
-    freq(pin1, 880, 500); // la
-    freq(pin1, 988, 250);
-    freq(pin1, 880, 250);
-    freq(pin1, 740, 250);
-    freq(pin1, 880, 500);
+    freq(pin1, 880, 250); // A5
+    freq(pin1, 880, 500); // A5
+    freq(pin1, 988, 250); // B5
+    freq(pin1, 880, 250); // A5
+    freq(pin1, 740, 250); // F#5
+    freq(pin1, 880, 500); // A5
         
-    freq(pin1, 880, 250); // la
-    freq(pin1, 880, 500); // la
-    freq(pin1, 988, 250);
-    freq(pin1, 880, 250);
-    freq(pin1, 740, 250);
-    freq(pin1, 880, 500);
+    freq(pin1, 880, 250); // A5
+    freq(pin1, 880, 500); // A5
+    freq(pin1, 988, 250); // B5
+    freq(pin1, 880, 250); // A5
+    freq(pin1, 740, 250); // F#5
+    freq(pin1, 880, 500); // A5
 
-    freq(pin1, 587, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 659, 250);
-    freq(pin1, 740, 750);
-    freq(pin1, 587, 250);
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 659, 250); // E5
+    freq(pin1, 740, 750); // F#5
+    freq(pin1, 587, 250); // D5
 
-    freq(pin1, 740, 750);
-    freq(pin1, 880, 250);
-    freq(pin1, 880, 1000);
+    freq(pin1, 740, 750); // F#5
+    freq(pin1, 880, 250); // A5
+    freq(pin1, 880, 1000); // A5
         
-    freq(pin1, 587, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 659, 250);
-    freq(pin1, 740, 1000);
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 659, 250); // E5
+    freq(pin1, 740, 1000); // F#5
 
-    freq(pin1, 587, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 659, 250);
-    freq(pin1, 740, 1000);
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 659, 250); // E5
+    freq(pin1, 740, 1000); //F#5
     
-    freq(pin1, 659, 250);
-    freq(pin1, 659, 250);
-    freq(pin1, 659, 250);
-    freq(pin1, 587, 250);
-    freq(pin1, 659, 500);
-    freq(pin1, 740, 500);
+    freq(pin1, 659, 250); // E5
+    freq(pin1, 659, 250); // E5
+    freq(pin1, 659, 250); // E5
+    freq(pin1, 587, 250); // D5
+    freq(pin1, 659, 500); // E5
+    freq(pin1, 740, 500); // F#5
 
-    freq(pin1, 880, 500);
-    freq(pin1, 784, 500);
-    freq(pin1, 740, 500);
-    freq(pin1, 659, 500);
+    freq(pin1, 880, 500); // A5
+    freq(pin1, 784, 500); // G5
+    freq(pin1, 740, 500); // F#5
+    freq(pin1, 659, 500); // E5
 }
 
 
